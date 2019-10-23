752ef45bd7be1c32ea43158c8f60a17f93e35ae6
Daniel Ryan <d@nielryan.com>
2019-10-23 18:32:59 +0200

    new version

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,4 @@
 import '../scss/main.scss';
-import './gallery';
 
-// Initialization of Foundation with JQuery
-import { Foundation } from 'foundation-sites/js/foundation.core';
-import $ from 'jquery';
-Foundation.addToJquery($);
-$(document).foundation();
+import AOS from 'aos';
+AOS.init();
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,74 @@
 // Webpack uses this to work with directories
 const path = require('path');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+var PUBLIC_DIR = path.resolve(__dirname, 'dist');
+var SRC_DIR = path.resolve(__dirname, 'src');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/js/app.js',
+  mode: process.env.NODE_ENV || 'development',
+  entry: SRC_DIR + '/js/app.js',
+  devtool: "source-map",
+
   // Path and filename of your result bundle.
   // Webpack will bundle all JavaScript into this file
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: PUBLIC_DIR,
     filename: 'bundle.js'
   },
 
   module: {
     rules: [
-      // { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
       {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
           'style-loader',
+          MiniCssExtractPlugin.loader,
+
           // Translates CSS into CommonJS
-          'css-loader',
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              autoprefixer: {
+                browsers: ["last 2 versions"]
+              },
+              sourceMap: true,
+              plugins: () => [autoprefixer]
+            },
+          },
           // Compiles Sass to CSS
-          'sass-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              sassOptions: {
+                includePaths: ['./node_modules']
+              }
+            }
+          },
+
         ],
       },
       {
         test: /\.(png|jpeg|otf|...)$/,
         use: [
-          { loader: 'url-loader', options: { limit: 8192, publicPath: './dist' } }
+          { loader: 'url-loader', options: { limit: 8192 } }
           // limit => file.size =< 8192 bytes ? DataURI : File
         ]
       }
     ],
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+  ],
 };
