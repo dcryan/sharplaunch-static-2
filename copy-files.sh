3f45ccaca4a090fc27df99fdc70defc3729a5101
Daniel Ryan <d@nielryan.com>
2019-10-29 23:11:56 +0100

    missed '/' in copy-files.sh

--- copy-files.sh
+++ copy-files.sh
@@ -5,7 +5,7 @@ rm -rf src/*
 rm index.html
 rm single.html
 
-cp -rf ~/Development/sharplaunch/app/View/Themed/Sharplaunch/webroot/dist* ./dist
+cp -rf ~/Development/sharplaunch/app/View/Themed/Sharplaunch/webroot/dist/* ./dist
 cp -rf ~/Development/sharplaunch/app/View/Themed/Sharplaunch/webroot/src/* ./src
 cp -rf ~/Development/sharplaunch/app/View/Themed/Sharplaunch/webroot/index.html .
 cp -rf ~/Development/sharplaunch/app/View/Themed/Sharplaunch/webroot/single.html .
