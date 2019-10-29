1e0f827e824fa49ea123f51e8d00740c7a4b2eee
Daniel Ryan <d@nielryan.com>
2019-10-29 23:08:37 +0100

    updated files

---
+++ copy-files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf dist/*
+rm -rf src/*
+rm index.html
+rm single.html
+
+cp -rf ~/Development/sharplaunch/app/View/Themed/Sharplaunch/webroot/dist* ./dist
+cp -rf ~/Development/sharplaunch/app/View/Themed/Sharplaunch/webroot/src/* ./src
+cp -rf ~/Development/sharplaunch/app/View/Themed/Sharplaunch/webroot/index.html .
+cp -rf ~/Development/sharplaunch/app/View/Themed/Sharplaunch/webroot/single.html .
