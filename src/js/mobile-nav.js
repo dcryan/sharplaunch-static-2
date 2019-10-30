8c09da8eace78d5e932bf3ba890dd67135f8cf82
Daniel Ryan <d@nielryan.com>
2019-10-30 17:01:16 +0100

    Fixed mobile nav, so it doesn't add space to the right side

--- src/js/mobile-nav.js
+++ src/js/mobile-nav.js
@@ -1,10 +1,23 @@
 
-$(function() {
-  $('.header__menu-button').click(function(e) {
-    e.preventDefault();
+function toggleMenu() {
+  $('.hamburger').toggleClass('active');
+  $('.mobile-menu').toggleClass('active');
+  $('body').toggleClass('menu-active');
+}
 
-    $('.hamburger').toggleClass('active');
-    $('.mobile-menu').toggleClass('active');
-    $('body').toggleClass('menu-active');
+$(function () {
+  $('.header__menu-button').click(function (e) {
+    e.preventDefault();
+    toggleMenu();
   });
+
+
+  $('.mobile-menu a, .mobile-menu button').click(function (e) {
+    // We don't want to preventDefault in this case.
+    // The user is probably going to the Client Portal, etc.
+    // But we still want to toggle the menu just in case.
+    // e.preventDefault();
+    toggleMenu();
+  });
+
 });
