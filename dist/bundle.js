8c7aeb06fc3d93627ab425dfeff12d2fddf1fc50
Daniel Ryan <d@nielryan.com>
2019-11-05 13:32:06 +0100

    updating with mobile menu and more button

--- dist/bundle.js
+++ dist/bundle.js
@@ -422,6 +422,76 @@ __webpack_require__.r(__webpack_exports__);
 
 aos__WEBPACK_IMPORTED_MODULE_2___default.a.init();
 
+$(function () {
+
+  window.onresize = setHeader;
+  var headerContainer = document.getElementsByClassName("header__container")[0];
+  var headerLogo = headerContainer.getElementsByClassName("header__logo")[0];
+  var navLinks = headerContainer.getElementsByClassName("header__nav-link");
+  var moreButton = headerContainer.getElementsByClassName("header__nav-more")[0];
+  var navLinkButton = headerContainer.getElementsByClassName("header__nav-button")[0];
+  var moreNavLinks = moreButton.getElementsByClassName("more__nav-link");
+
+  var navLinkWidths = getHeaderWidths(navLinks);
+
+
+  function setHeader() {
+    hideAll();
+
+    var localWidth = headerLogo.clientWidth + navLinkButton.clientWidth;
+    var navLinksContainerWidth = headerContainer.clientWidth - localWidth;
+
+    for (let i = 0; i < navLinkWidths.length; i++) {
+      var width = navLinkWidths[i];
+      localWidth += width;
+
+      if (localWidth > navLinksContainerWidth) {
+        // Needs "more"
+        setMore(i);
+        break;
+      }
+
+      navLinks[i].style.display = 'inherit';
+    }
+  }
+
+  function setMore(indexForRemaining) {
+    moreButton.style.display = "inherit";
+
+    for (let i = indexForRemaining; i < moreNavLinks.length; i++) {
+      moreNavLinks[i].style.display = 'inherit';
+    }
+  }
+
+  function hideAll() {
+    moreButton.style.display = "none";
+
+    for (let i = 0; i < navLinks.length; i++) {
+      navLinks[i].style.display = "none";
+    }
+
+    for (let i = 0; i < moreNavLinks.length; i++) {
+      moreNavLinks[i].style.display = "none";
+    }
+  }
+
+  function getHeaderWidths(navLinks) {
+    var widths = [];
+    for (let i = 0; i < navLinks.length; i++) {
+      widths.push(navLinks[i].clientWidth);
+    }
+
+    return widths;
+  }
+
+  setHeader();
+
+
+  $(".header__nav-more button").click(function(e) {
+    e.preventDefault();
+    $(".header__nav-more-dropdown").toggleClass("active");
+  });
+});
 
 /***/ }),
 
